821175b2a5125cc1b378895d27109ab3c0e00c13
Pedro <Pedro@hdeifler>
2023-10-03 15:02:00 -0300

    Votacao.class.php

--- classes/votacao.class.php
+++ classes/votacao.class.php
@@ -32,43 +32,79 @@ class Votacao{
 
     public function setnome($nome){
         $this->nome = $nome;
-}
+    }
 
-public function getnome(){
-    return $this->nome;
-}
+    public function getnome(){
+        return $this->nome;
+    }
 
-public function setvoto($voto){
-    $this->voto = $voto;
-}
+    public function setvoto($voto){
+        $this->voto = $voto;
+    }
 
-public function getvoto(){
-return $this->voto;
-}
+    public function getvoto(){
+    return $this->voto;
+    }
 
-public function setchapa($chapa){
-    $this->chapa = $chapa;
-}
+    public function setchapa($chapa){
+        $this->chapa = $chapa;
+    }
 
-public function getchapa(){
-return $this->chapa;
-}
+    public function getchapa(){
+    return $this->chapa;
+    }
 
-public function setusuario($usuario){
-    $this->usuario = $usuario;
-}
+    public function setusuario($usuario){
+        $this->usuario = $usuario;
+    }
 
-public function getusuario(){
-return $this->usuario;
-}
+    public function getusuario(){
+    return $this->usuario;
+    }
 
-public function setdata($data){
-    $this->data = $data;
-}
+    public function setdata($data){
+        $this->data = $data;
+    }
 
-public function getdata(){
-return $this->data;
-}
+    public function getdata(){
+    return $this->data;
+    }
+
+    public function inserir(){
+        $sql = 'INSERT INTO usuario(nome, voto)
+                     VALUES (:nome, :voto)';
+        $params = array(
+        ':nome'=>$this->getnome(),
+        ':voto' => $this->getvoto(),
+        ':chapa' => $this->getchapa(),
+        'usuario' => $this->getusuario(),
+        'data' => $this->getdata()
+    );
+       
+       return Database::executar($sql, $params);
+    }
+
+    public function excluir($id){
+        $sql = 'DELETE FROM votacao 
+                 WHERE id = :id';         
+        $params = array(':id'=>$id);         
+        return Database::executar($sql, $params);
+    }
+
+    public function editar(){
+        $sql = 'UPDATE votacao
+                   SET nome = :nome, 
+                    voto = :voto,
+                    chapa = :chapa,
+                    usuario = :usuario,
+                    data = :data
+                 WHERE   id = :id';
+        $params = array(':id'=>$this->getid(),
+                        ':nome'=> $this->getnome());
+       return Database::executar($sql, $params);
+       
+    }
+
 }
 
 ?>
