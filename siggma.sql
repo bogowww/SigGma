26b695b7f1301c9a459ed7b066652d670ba8686e
PabloMachadoIFC <109369923+PabloMachadoIFC@users.noreply.github.com>
2023-10-03 14:50:11 -0300

    Update siggma.sql

--- siggma.sql
+++ siggma.sql
@@ -1,20 +1,23 @@
-CREATE TABLE IF NOT EXISTS `siggma`.`usuario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NULL,
-  `matricula` INT NULL,
-  PRIMARY KEY (`id`))
+-- Criação da tabela 'usuario'
+CREATE TABLE IF NOT EXISTS siggma.usuario (
+  id INT AUTO_INCREMENT NOT NULL,
+  nome VARCHAR(45) NULL,
+  matricula INT NULL,
+  PRIMARY KEY (id)
+);
 
-
-CREATE TABLE IF NOT EXISTS `siggma`.`votacao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NULL,
-  `voto` INT NULL,
-  `chapa` VARCHAR(45) NULL,
-  `data` DATE NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_votacao_usuario`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `siggma`.`usuario` (`id`)
+-- Criação da tabela 'votacao'
+CREATE TABLE IF NOT EXISTS siggma.votacao (
+  id INT AUTO_INCREMENT NOT NULL,
+  nome VARCHAR(45) NULL,
+  voto INT NULL,
+  chapa VARCHAR(45) NULL,
+  data DATE NULL,
+  usuario_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (usuario_id) 
+    REFERENCES siggma.usuario (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE NO ACTION,
+  INDEX fk_votacao_usuario_idx (usuario_id)
+);
