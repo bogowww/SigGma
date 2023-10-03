eb8940144c295fe8eb7289859c0b6797a8076291
PabloMachadoIFC <109369923+PabloMachadoIFC@users.noreply.github.com>
2023-10-03 15:02:41 -0300

    Update crud.class.php

--- classes/crud.class.php
+++ classes/crud.class.php
@@ -1,10 +1,25 @@
 <?php
-abstract class CRUD{
-    private $id;
+    require_once('database.class.php');
+    abstract class CRUD{
+        
+        private $id = 0;
+        
+        public function __construct($pid){
+            $this->setId($pid);
+        }        
+        
+        public function getId(){
+            return $this->id;
+        }
+        
+        public function setId($id){
+            $this->id = $id;
+        }       
 
-    public abstract function inserir();
-    public abstract function excluir();
-    public abstract function editar();
-    public abstract function listar();
-}
+        public abstract function inserir();
+        
+        public abstract function excluir();
+        
+        public abstract function editar();
+    }
 ?>
