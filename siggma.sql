3050a297615be6850e35834d3203b89eb1e76741
ig1or <115035126+ig1or@users.noreply.github.com>
2023-10-03 16:51:43 -0300

    melhorado

--- siggma.sql
+++ siggma.sql
@@ -1,26 +1,37 @@
--- Criação do schema 'SigGma'
-CREATE SCHEMA IF NOT EXISTS siggma;
 
--- Criação da tabela 'usuario'
-CREATE TABLE IF NOT EXISTS siggma.usuario (
-  id INT AUTO_INCREMENT NOT NULL,
-  nome VARCHAR(45) NULL,
-  matricula INT NULL,
-  PRIMARY KEY (id)
-);
+/*Tabela de tipoUsuario*/
 
--- Criação da tabela 'votacao'
-CREATE TABLE IF NOT EXISTS siggma.votacao (
-  id INT AUTO_INCREMENT NOT NULL,
-  nome VARCHAR(45) NULL,
-  voto INT NULL,
-  chapa VARCHAR(45) NULL,
-  data DATE NULL,
-  usuario_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (usuario_id) 
-    REFERENCES siggma.usuario (id)
+CREATE TABLE IF NOT EXISTS `siggma`.`tipoUsuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+/*Tabela de Usuario*/
+
+CREATE TABLE IF NOT EXISTS `siggma`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NULL,
+  `matricula` INT NULL,
+  `tipoUsuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_usuario_tipoUsuario1`
+    FOREIGN KEY (`tipoUsuario_id`)
+    REFERENCES `siggma`.`tipoUsuario` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  INDEX fk_votacao_usuario_idx (usuario_id)
-);
+    ON UPDATE NO ACTION);
+
+/*Tabela de Votação*/
+
+CREATE TABLE IF NOT EXISTS `siggma`.`votacao` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NULL,
+  `voto` INT NULL,
+  `chapa` VARCHAR(45) NULL,
+  `data` DATE NULL,
+  `usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_votacao_usuario`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `siggma`.`usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
