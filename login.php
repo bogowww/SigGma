55490a2555918a04fd5827647fa9b4a275e8cb4d
NicolasDarian <115034967+NicolasDarian@users.noreply.github.com>
2023-11-08 15:08:17 -0300

    Classe adicionada
    Classe adicionada

--- login.php
+++ login.php
@@ -1,32 +1,55 @@
+<?php
+require_once('classes/usuario.class.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $usuarios = Usuario::listar();
+
+
+    var_dump($_POST);
+    foreach ($usuarios as $usuario) {
+        if ($usuario['nome'] === $username && $usuario['senha'] === $password) {
+            header('Location: chapa.php');
+            exit();
+        }
+    }
+    header('Location: login.php?error=1');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/login_cadastro.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+    </style>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 col-sm-12">
-                <h2 class="text-center" id="spaceTitleLogin">Entre na sua conta</h2>
+                <h2 class="text-center" id="spaceTitle">Entre na sua conta</h2>
                 <div class="d-flex justify-content-center">
-                    <form action="" method="post">
+                    <form action="login.php" method="post">
                         <div class="mb-4">
-                            <input class="form-control input" type="text" placeholder="Nome de usuário">
+                            <input class="form-control" type="text" id="input" name="username" placeholder="Nome de usuário">
                         </div>
                         <div class="mb-3">
-                            <input class="form-control input" type="password"  placeholder="Senha">
+                            <input class="form-control" type="password" id="input" name="password" placeholder="Senha">
                         </div>
                         <div class="d-flex justify-content-end mb-5">
-                            <a href="#">Esqueci a senha</a>
+                            <a href="senha.php">Esqueci a senha</a>
                         </div>
                         <div class="d-grid col-8 mx-auto" id="space">
                             <button class="button">Entrar</button>
                         </div>
                         <div class="d-flex justify-content-center mt-5">
-                            <p>Não tem conta? <a href="cadastro.html">Clique aqui para fazer cadastro.</a></p>
+                            <p>Não tem conta? <a href="cadastro.php">Clique aqui para fazer cadastro.</a></p>
                         </div>
                     </form>
                 </div>
