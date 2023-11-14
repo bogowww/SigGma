4849b2421785610b316b662e0141f434851cf0fc
NicolasDarian <115034967+NicolasDarian@users.noreply.github.com>
2023-11-14 14:45:06 -0300

    Create chapa.php
    Create chapa.php

---
+++ chapa.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/chapa.css">
+    
+
+    <title>Menu Exemplo</title>
+</head>
+<body>
+    <?php include 'menu.php' ?>
+    <h1 class="h1">CHAPAS INSCRITAS</h1>
+    <br><br>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="square-container">
+                <div class="square-content">
+                    <img src="img/upload.png"><br>
+                </div>
+            </div>
+            <br><br>
+            <h2 class="h2">NOME DA CHAPA</h2>
+            <br><br>
+            <h3 class="texto">Gabriel da Cunha Barbosa, Inti Bohmann Leite da Luz, Gustavo Preilipper, Victor Hugo Reinicke, Guilherme Cellarius Dubiela, Nicole Rezena, Laila Vitória Marconatto, Gustavo Gomes Giancristofaro, Alison Nazário, Pedro H. Bezerra dos Santos.</h3>
+        </div>
+        <div class="col-md-6">
+            <div class="square-container">
+                <div class="square-content">
+                    <img src="img/upload.png"><br>
+                </div>
+            </div>
+            <br><br>
+            <h2 class="h2">NOME DA CHAPA</h2>
+            <br><br>
+            <h3 class="texto">Gabriel da Cunha Barbosa, Inti Bohmann Leite da Luz, Gustavo Preilipper, Victor Hugo Reinicke, Guilherme Cellarius Dubiela, Nicole Rezena, Laila Vitória Marconatto, Gustavo Gomes Giancristofaro, Alison Nazário, Pedro H. Bezerra dos Santos.</h3>
+        </div>
+    </div><br><br><br>
+    <div class="spacer"></div>
+        <div class="texto-fora-container">
+        <p class="text">Para votar é necessário ter CADASTRO! <a href="login.php">Fazer Login</a></p> 
+    </div>
+
+    <button class="button">VOTAR</button>
+</body>
+
+</html>
