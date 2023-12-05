cde56ed81da09c499c8a494483d74144e5c2a2cb
gustavo <gustavof.felipe.ifc.riodosul@gmail.com>
2023-12-05 15:22:04 -0300

    Atualizando página de votação de chapa e menu para baixar pdf

---
+++ teste.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta name="generator" content="Aspose.Words for .NET 23.11.0" />
+    <title></title>
+    <style type="text/css">
+        body {
+            font-family: 'Times New Roman';
+            font-size: 12pt;
+        }
+        
+        p {
+            margin: 0pt;
+        }
+    </style>
+</head>
+<body>
+        <p style="margin-top:25.5pt; margin-left:218.2pt; text-align:justify; line-height:20.75pt">
+            <span style="font-family:Calibri; font-size:17pt">CHAPAS</span>
+            <span style="font-family:Calibri; font-size:17pt; letter-spacing:1.8pt"> </span>
+            <span style="font-family:Calibri; font-size:17pt">INSCRITAS</span>
+        </p>
+        <p style="margin-top:172.65pt; margin-left:96.5pt; text-align:justify; line-height:12.2pt">
+            <span style="font-family:Calibri; font-size:10pt">NOME</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:4.8pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">DA</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:0.5pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">CHAPA</span>
+        </p>
+        <p style="margin-top:46.85pt; margin-left:11.9pt; text-align:justify; line-height:12.2pt">
+            <span style="font-family:Calibri; font-size:10pt">Para</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:1.9pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">votar</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:3.6pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">é necessário</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:5pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">ter</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:1.3pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">CADASTRO!</span>
+            <span style="font-family:Calibri; font-size:10pt; letter-spacing:13.55pt"> </span>
+            <span style="font-family:Calibri; font-size:10pt">Fazerlogin.</span>
+        </p>
+    </div>
+</body>
+</html>
