b6ffcd96b5b0426c0f9075c795aec1b72bc2e5b2
NicolasDarian <115034967+NicolasDarian@users.noreply.github.com>
2023-12-06 13:27:10 -0300

    Update chapa.php

--- chapa.php
+++ chapa.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="assets/menu.css">
-    <link rel="stylesheet" href="assets/chapa.css">
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/chapa.css">
     
 
 
