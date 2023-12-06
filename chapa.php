fcf000322091690ca7caccb68e2ebb6268b211b5
NicolasDarian <115034967+NicolasDarian@users.noreply.github.com>
2023-12-06 13:14:46 -0300

    Update

--- chapa.php
+++ chapa.php
@@ -7,11 +7,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="assets/css/menu.css">
-    <link rel="stylesheet" href="assets/css/chapa.css">
+    <link rel="stylesheet" href="assets/menu.css">
+    <link rel="stylesheet" href="assets/chapa.css">
     
 
-    <title>Menu Exemplo</title>
+
 </head>
 <body>
     <?php include 'menu.php' ?>
@@ -40,13 +40,41 @@
             <br><br>
             <h3 class="texto">Gabriel da Cunha Barbosa, Inti Bohmann Leite da Luz, Gustavo Preilipper, Victor Hugo Reinicke, Guilherme Cellarius Dubiela, Nicole Rezena, Laila Vitória Marconatto, Gustavo Gomes Giancristofaro, Alison Nazário, Pedro H. Bezerra dos Santos.</h3>
         </div>
-    </div><br><br><br>
-    <div class="spacer"></div>
-        <div class="texto-fora-container">
-        <p class="text">Para votar é necessário ter CADASTRO! <a href="login.php">Fazer Login</a></p> 
+        </div><br><br><br>
+</div>
+        <p class="text">Para votar é necessário ter CADASTRO! <a href="login.php">Fazer Login</a></p>
+
+    <div class="texto-fora-container">
+            <button class="button" data-bs-toggle="modal" data-bs-target="#votarModal">VOTAR</button>
+        </div>
     </div>
 
-    <button class="button">VOTAR</button>
+    <div class="modal fade" id="votarModal" tabindex="-1" aria-labelledby="votarModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-body">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+            <div class="square-container">
+                <div class="square-content">
+                <img src="img/upload.png"><br>
+                <h6 class="fontchapa"> CHAPA 01</h6>
+                </div>
+                </div><br><br><br>
+                
+            <input type="checkbox" name="" id="" class="fontc" h6 class="fontc">    Chapa 1</h6>
+            </div>
+    
+
+
+
+
+            </div> 
+        </div>
+    </div>
+</div>
 </body>
+</html>
 
 </html>
