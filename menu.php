b589e0e32aafc4d058be4482b1a9313f3e37a5b2
NicolasDarian <115034967+NicolasDarian@users.noreply.github.com>
2023-12-06 13:12:50 -0300

    Atualização

--- menu.php
+++ menu.php
@@ -1,3 +1,10 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/menu.css">
+</head>
 <nav class=" body navbar navbar-expand-lg">
         <li class="font menu-item">
             <img src="img/logomenu.png" alt="Ícone" />
@@ -5,7 +12,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="nav-item" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="font nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Sobre</a>
@@ -13,15 +20,20 @@
                 <li class="font nav-item">
                     <a class="nav-link" href="#">Arrecadação</a>
                 </li>
-                <li class="font nav-item">
+                <li class=" font ">
                     <a class="nav-link" href="#">Votação</a>
                 </li>
                 <li class="font nav-item">
                     <a class="nav-link" href="#">Contato</a>
                 </li>
+                <li class="font nav-item">
+            <a class="nav-link" href="pdf/pdfSiGma.pdf" download>Manual</a>
+                </li>
             </ul>
         </div>
     </nav>
-    <img class="image-below-menu" src="img/tela.png" alt="Imagem abaixo do menu">
+        <img class="image-below-menu" src="img/tela.png" alt="Imagem abaixo do menu">
     <div class="container">
     <br><br>
+</body>
+</html>
